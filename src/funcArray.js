d0759578ca7c39d0b9e894c12805b61b4096a402
Joel <joelv2@gmail.com>
2022-11-15 22:54:39 +0100

    Katas 1 a 7 terminadas

--- src/funcArray.js
+++ src/funcArray.js
@@ -8,16 +8,6 @@ export function maxOfTwoNumbers(num1, num2){
     }
 }
 
-let words = [
-  'mystery',
-  'brother',
-  'aviator',
-  'crocodile',
-  'pearl',
-  'orchard',
-  'crackpot'
-];
-
 
 export function findLongestWord(wordsArray){
     let arrayOriginal = wordsArray;
@@ -31,18 +21,153 @@ export function findLongestWord(wordsArray){
 
 }
 
-let numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
 export function sumArray(numbersArray){
-    // return numbersArray.reduce();
+    
+    if(numbersArray == undefined || numbersArray == 0) {
+        return 0;
+    }else {
+            let suma = numbersArray.reduce((acc,numero) => acc + numero);
+        return suma;
+    }
+
+
+
 }
 
-export function averageNumbers(){}
-export function averageWordLength(){}
-export function uniquifyArray(){}
-export function doesWordExist(){}
-export function howManyTimes(){}
-export function greatestProduct(){}
+export function averageNumbers(numbersArray){
+
+    if(numbersArray == undefined || numbersArray == 0) {
+        return undefined;
+    }else {
+            
+        return sumArray(numbersArray) / numbersArray.length;
+    }
+
+    
+
+}
+
+let palabras = [
+    'seat',
+    'correspond',
+    'linen',
+    'motif',
+    'hole',
+    'smell',
+    'smart',
+    'chaos',
+    'fuel',
+    'palace'
+];
+
+export function averageWordLength(array){
+    
+    let arrayPalabras = [];
+
+    array.forEach(word => arrayPalabras.push(word.length));
+
+    if(array === undefined || array.length == 0) {
+        return undefined;
+    }else {
+        let suma = arrayPalabras.reduce((acc,numero) => acc + numero);
+        return suma / array.length;
+    }
+
+
+}
+export function uniquifyArray(arr){
+
+    if (arr !== undefined || arr.length != 0) {
+        
+        let arrayPalabras = [];
+
+        arr.forEach(word => {
+
+            if (!arrayPalabras.includes(word)) {
+                arrayPalabras.push(word);
+            }
+        
+        });
+
+        if (arr === undefined || arr.length == 0 ) {
+            return undefined;
+        }else{
+            return arrayPalabras;
+        }
+
+
+    }else {
+        return undefined;
+    }
+}
+
+export function doesWordExist(arr, word){
+    
+    if (arr !== undefined || arr.length != 0) {
+        
+        let existe;
+        
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === word){
+                existe = true;
+            }
+        }
+
+        if (existe === undefined ) {
+            return false;
+        }else{
+            return true
+        }
+
+
+    }else {
+        return undefined;
+    }
+
+
+}
+
+
+export function howManyTimes(arr, word) {
+    if (arr.length === 0 ) {
+        
+        return undefined; //la descripción del test está mal, espera undefined, no false
+
+    }else if (arr !== undefined) {
+        let cuenta = 0;
+        
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === word){
+                cuenta++;
+            }
+        }
+
+        if (cuenta == 0 ) {
+            return 0;
+        }else{
+            return cuenta;
+        }
+    }
+}
+
+export function greatestProduct(matriz){
+
+    let nuevoArray = [];
+
+    let matrizOpDerechaArriba = 0;
+    let matrizOpDerecha = 0;
+    let matrizOpDiagonalAbajo = 0;
+    let matrizOpAbajo = 0;
+
+    for (let i = 0; i < matriz.length; i++) { 
+        for (let j = 0; j < matriz[i].length; j++) { 
+        
+        }
+    }
+
+
+}
 
 
 
