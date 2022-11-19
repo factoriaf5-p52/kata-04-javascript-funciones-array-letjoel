c9d910664553161cec19e83304c48199a16ffcd7
Joel Herrero <joelv2@gmail.com>
2022-11-19 01:58:28 +0100

    Kata Bonus terminada!

--- src/funcArray.js
+++ src/funcArray.js
@@ -151,20 +151,126 @@ export function howManyTimes(arr, word) {
     }
 }
 
-export function greatestProduct(matriz){
+export function greatestProduct(matrix){
 
-    let nuevoArray = [];
+    //Sacamos filas
+    let filas = 0;
+    matrix.forEach( fila => filas++);
 
-    let matrizOpDerechaArriba = 0;
-    let matrizOpDerecha = 0;
-    let matrizOpDiagonalAbajo = 0;
-    let matrizOpAbajo = 0;
-
-    for (let i = 0; i < matriz.length; i++) { 
-        for (let j = 0; j < matriz[i].length; j++) { 
-        
-        }
+    //Sacamos columnas
+    let columnas = 0;
+    for (let i = 0; i < 1; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            columnas++;
+        }  
     }
+
+    //Creamos arrays vacios para rellenarlos luego
+
+    let matrixDiagonalArriba = [];
+    let matrixDerecha = [];
+    let matrixDiagonalAbajo = [];
+    let matrixAbajo = [];
+
+    // let matrixDiagonalArriba = new Array(filas);
+    // for (let i = 0; i < matrixDiagonalArriba.length; i++) {
+    //   matrixDiagonalArriba[i] = new Array(columnas);
+    // };
+
+    // let matrixDerecha = new Array(filas);
+    // for (let i = 0; i < matrixDerecha.length; i++) {
+    //   matrixDerecha[i] = new Array(columnas);
+    // };
+
+    // let matrixDiagonalAbajo = new Array(filas);
+    // for (let i = 0; i < matrixDiagonalAbajo.length; i++) {
+    //   matrixDiagonalAbajo[i] = new Array(columnas);
+    // };
+    // let matrixAbajo = new Array(filas);
+    // for (let i = 0; i < matrixAbajo.length; i++) {
+    //   matrixAbajo[i] = new Array(columnas);
+    // };
+
+    //Recorremos matriz original
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+
+            //Diagonal Arriba
+            if (i >= 3 && j <= (columnas - 4)) {
+                let value = matrix[i][j];
+                let value2 = matrix[i-1][j+1];
+                let value3 = matrix[i-2][j+2];
+                let value4 = matrix[i-3][j+3];
+                let diagonalArribaProduct = value * value2 * value3 * value4;
+                matrixDiagonalArriba.push(diagonalArribaProduct);
+                
+            }else {
+                let val = matrix[i][j];
+                matrixDiagonalArriba.push(val); 
+            }
+
+            
+
+            //Derecha
+            if (j <= (columnas - 4)) {
+                let value = matrix[i][j];
+                let value2 = matrix[i][j+1];
+                let value3 = matrix[i][j+2];
+                let value4 = matrix[i][j+3];
+
+                let derechaProduct = value * value2 * value3 * value4;
+                matrixDerecha.push(derechaProduct);
+                
+            }else {
+                let val = matrix[i][j];
+                matrixDerecha.push(val); 
+            }
+
+            //Diagonal abajo
+            if (i <= (filas - 4) && j <= (columnas - 4)) {
+                let value = matrix[i][j];
+                let value2 = matrix[i+1][j+1];
+                let value3 = matrix[i+2][j+2];
+                let value4 = matrix[i+3][j+3];
+
+                let diagonalAbajoProduct = value * value2 * value3 * value4;
+                matrixDiagonalAbajo.push(diagonalAbajoProduct);
+                
+            }else {
+                let val = matrix[i][j];
+                matrixDiagonalAbajo.push(val); 
+            }
+
+            //Abajo
+            if (i <= (filas - 4)) {
+                let value = matrix[i][j];
+                let value2 = matrix[i+1][j];
+                let value3 = matrix[i+2][j];
+                let value4 = matrix[i+3][j];
+
+                let abajoProduct = value * value2 * value3 * value4;
+                matrixAbajo.push(abajoProduct);
+                
+            }else {
+                let val = matrix[i][j];
+                matrixAbajo.push(val); 
+            }
+
+
+
+        }  
+    }
+
+    console.log(matrixDiagonalArriba);
+
+    let arrayFinal = [];
+    arrayFinal.push(Math.max(...matrixDiagonalArriba));
+    arrayFinal.push(Math.max(...matrixDerecha));
+    arrayFinal.push(Math.max(...matrixDiagonalAbajo));
+    arrayFinal.push(Math.max(...matrixAbajo));
+    console.log(arrayFinal);
+    console.log(Math.max(...arrayFinal));
+    return Math.max(...arrayFinal);
 
 
 }
